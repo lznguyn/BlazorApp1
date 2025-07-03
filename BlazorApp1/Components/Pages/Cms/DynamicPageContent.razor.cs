b89bce61e37ec294aa15aa06d89ecf1292236c0c
Văn Thạnh <117495750+thnak@users.noreply.github.com>
2025-07-03 15:46:22 +0700

    Add DynamicPageContent component with routing and parameter support

---
+++ BlazorApp1/Components/Pages/Cms/DynamicPageContent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components.Pages.Cms;
+
+[Route("/cms/content/{Id}")]
+public partial class DynamicPageContent : ComponentBase
+{
+    [Parameter]
+    public string Id { get; set; } = string.Empty;
+}
